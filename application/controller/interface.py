2b9c3f70c29368dc3ab21a9d10a3d6fe386de94f
lichengbo <lichengbo@huishoubao.com.cn>
2018-04-09 15:42:22 +0800

    新增脚本接口数据库处理的API

--- application/controller/interface.py
+++ application/controller/interface.py
@@ -2,3 +2,42 @@ from flask import request, jsonify
 
 from application import app
 from application.api import interface as InterfaceAPI
+
+
+@app.route('/interface/add', methods=['POST'])
+def add_interface():
+    try:
+        InterfaceAPI.add_interface(**request.get_json())
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+    return jsonify({'success': True})
+
+
+@app.route('/interface/info', methods=['POST'])
+def get_interface():
+    try:
+        results = InterfaceAPI.get_interface(**request.get_json())
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+    interface_list = []
+    for interface in results:
+        interface_list.append(interface.to_dict())
+    return jsonify(interface_list)
+
+
+@app.route('/interface/update', methods=['POST'])
+def modify_interface():
+    try:
+        InterfaceAPI.modify_interface(**request.get_json())
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+    return jsonify({'success': True})
+
+
+@app.route('/interface/delete', methods=['POST'])
+def delete_interface():
+    try:
+        InterfaceAPI.del_interface(**request.get_json())
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+    return jsonify({'success': True})
