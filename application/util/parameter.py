3537ce67a313e9d312a9c933999162cc824c4732
Yuan Liu <liuyuan900811@gmail.com>
2018-04-11 17:40:06 +0800

    字符串内参数搜索工具

---
+++ application/util/parameter.py
@@ -0,0 +1,15 @@
+import re
+
+
+def search_parameter(input_string):
+    """
+    在给定字符串中查找被 ${} 包裹的所有参数, 并以列表返回
+    :param input_string:        给定用来搜索参数的字符串
+    :return:                    参数列表
+    """
+    pattern = re.compile(r'\${[^$]*}')
+    match_result = pattern.findall(input_string)
+    param_list = []
+    for item in match_result:
+        param_list.append(item.replace('${', '').replace('}', ''))
+    return param_list
