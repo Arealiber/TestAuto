be34ba02a9c9a3daed4058d9a8fc6b007539ab5c
Yuan Liu <liuyuan900811@gmail.com>
2018-04-12 10:40:12 +0800

    static path and url config

--- application/__init__.py
+++ application/__init__.py
@@ -11,6 +11,8 @@ web_root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
 
 app = Flask(
     __name__,
+    static_folder=os.path.abspath(os.path.join(web_root, 'static')),
+    static_url_path="",
     template_folder=os.path.abspath(os.path.join(web_root, 'templates'))
 )
 app.config.from_object(default)
