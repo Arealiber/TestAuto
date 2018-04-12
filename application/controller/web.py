318603f6787a35dea54025cc29d882c6ec7b0213
Yuan Liu <liuyuan900811@gmail.com>
2018-04-12 15:51:02 +0800

    templates creation

--- application/controller/web.py
+++ application/controller/web.py
@@ -6,3 +6,28 @@ from application import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/parameter')
+def parameter():
+    return render_template('parameter.html')
+
+
+@app.route('/interface')
+def interface():
+    return render_template('interface.html')
+
+
+@app.route('/use_case')
+def use_case():
+    return render_template('use_case.html')
+
+
+@app.route('/batch')
+def batch():
+    return render_template('batch.html')
+
+
+@app.route('/run_log')
+def run_log():
+    return render_template('run_log.html')
