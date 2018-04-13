27dde08523edd32d324faaaca316fa8d929c0a81
Yuan Liu <liuyuan900811@gmail.com>
2018-04-13 09:36:53 +0800

    前后端分离

--- application/controller/web.py
+++ application/controller/web.py
@@ -1,33 +1,34 @@
 from flask import render_template
 
 from application import app
+from application.api import interface as InterfaceAPI
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return app.send_static_file('index.html')
 
 
 @app.route('/parameter')
-def parameter():
-    return render_template('parameter.html')
+def parameter_index():
+    return app.send_static_file('parameter.html')
 
 
 @app.route('/interface')
-def interface():
-    return render_template('interface.html')
+def interface_index():
+    return app.send_static_file('interface.html')
 
 
 @app.route('/use_case')
-def use_case():
-    return render_template('use_case.html')
+def use_case_index():
+    return app.send_static_file('use_case.html')
 
 
 @app.route('/batch')
-def batch():
-    return render_template('batch.html')
+def batch_index():
+    return app.send_static_file('batch.html')
 
 
 @app.route('/run_log')
-def run_log():
-    return render_template('run_log.html')
+def run_log_index():
+    return app.send_static_file('run_log.html')
