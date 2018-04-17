b60508018fe968baa6338c08c950638ecb978461
Yuan Liu <liuyuan900811@gmail.com>
2018-04-17 11:28:10 +0800

    url更新

--- application/controller/web.py
+++ application/controller/web.py
@@ -12,7 +12,7 @@ def parameter_page(page_num):
 
 
 @app.route('/parameter_detail/<parameter_id>')
-def parameter_detail(parameter_id):
+def parameter_detail_page(parameter_id):
     return app.send_static_file('parameter_detail.html')
 
 
@@ -22,18 +22,28 @@ def interface_page(page_num):
 
 
 @app.route('/interface_detail/<interface_id>')
-def interface_detail(interface_id):
+def interface_detail_page(interface_id):
     return app.send_static_file('interface_detail.html')
 
 
-@app.route('/use_case')
-def use_case_index():
+@app.route('/use_case/<page_num>')
+def use_case_page(page_num):
     return app.send_static_file('use_case.html')
 
 
-@app.route('/batch')
-def batch_index():
+@app.route('/use_case_detail/<use_case_id>')
+def use_case_detail_page(use_case_id):
+    return app.send_static_file('use_case_detail.html')
+
+
+@app.route('/batch/<page_num>')
+def batch_page(page_num):
     return app.send_static_file('batch.html')
+
+
+@app.route('/batch_detail/<batch_id>')
+def batch_detail_page(batch_id):
+    return app.send_static_file('batch_detail.html')
 
 
 @app.route('/run_log')
