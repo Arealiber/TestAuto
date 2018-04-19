eea0617c7f5130dca5393e11a01acb1ee5b505e6
lichengbo <lichengbo@huishoubao.com.cn>
2018-04-19 18:06:33 +0800

    更新run_log查询接口

---
+++ application/controller/run_log.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import time
+from flask import request, jsonify
+from application import app
+from application.api import run_log as RunLogAPI
+
+current_time = time.strftime('%Y%m')
+
+
+if RunLogAPI.current_time == current_time:
+    RunLogAPI, = RunLogAPI.reload_import_module('application.api', run_log='run_log')
+
+
+@app.route('/run_log/add', methods=['POST'])
+def get_use_case_run_log():
+    """
+
+    :return:
+    """
+    try:
+        RunLogAPI.add_use_case_run_log(**request.get_json())
+    except Exception as e:
+        return jsonify({'success': False, 'res': str(e)})
+    return jsonify({'success': True})
+
+
