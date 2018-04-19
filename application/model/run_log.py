8024a99a896c8b4f85659ea53fc242211c37eb7e
lichengbo <lichengbo@huishoubao.com.cn>
2018-04-19 10:21:13 +0800

    更新优化日志数据库

--- application/model/run_log.py
+++ application/model/run_log.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 from application import engine
@@ -8,7 +8,7 @@ from application import engine
 Base = declarative_base()
 
 
-class TestCaseRunLog(Base):
+class UseCaseRunLog(Base):
     __tablename__ = 'test_case_run_log'
     __table_args__ = {
         "mysql_engine": "InnoDB",
@@ -16,17 +16,18 @@ class TestCaseRunLog(Base):
     }
 
     id = Column(Integer, primary_key=True)
-    test_case_name = Column(String(100), nullable=False)
+    use_case_name = Column(String(100), nullable=False)
     is_pass = Column(Boolean, nullable=False)
-    run_time = Column(DateTime, default=datetime.utcnow)
-    cost_time = Column(Integer, default=0, nullable=False)
+    create_time = Column(DateTime, default=datetime.utcnow)
+    run_time = Column(Float, default=0, nullable=False)
     status = Column(Integer, default=1, nullable=False)
 
     def to_dict(self):
         return{
             'id': self.id,
-            'test_case_name': self.test_case_name,
+            'use_case_id': self.use_case_id,
             'is_pass': self.is_pass,
+            'create_time': self.create_time,
             'run_time': self.run_time
         }
 
@@ -39,19 +40,24 @@ class RelationInterfaceRunLog(Base):
     }
 
     id = Column(Integer, primary_key=True)
-    relation_id = Column(Integer, nullable=False)  # 关联的testcase运行的日志id
-    interface_name = Column(String(100), nullable=False)
-    run_res = Column(String(1000), nullable=False)
-    run_time = Column(DateTime, default=datetime.utcnow)
+    use_case_run_log_id = Column(Integer, nullable=False)  # 关联的testcase运行的日志id
+    interface_id = Column(Integer, nullable=False)
+    res_status_code = Column(Integer, nullable=False)
+    res_headers = Column(String(1000), nullable=False)
+    res_payload = Column(String(10000), nullable=False)
+    is_pass = Column(Boolean, nullable=False)
+    create_time = Column(DateTime, default=datetime.utcnow)
     status = Column(Integer, default=1, nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'relation_id': self.relation_id,
-            'interface_name': self.interface_name,
-            'run_res': self.run_res,
-            'run_time': self.run_time
+            'use_case_run_log_id': self.use_case_run_log_id,
+            'interface_id': self.interface_id,
+            'res_status_code': self.res_status_code,
+            'res_headers': self.res_headers,
+            'res_payload': self.res_payload,
+            'is_pass': self.is_pass
         }
 
 
