2a6b94d7fa5725f8e4133395e971c62e71f57735
lichengbo <lichengbo@huishoubao.com.cn>
2018-04-23 17:42:37 +0800

    添加创建数据时放回值处理

--- application/api/run_log.py
+++ application/api/run_log.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from application.util.decorator import *
+from application.model import run_log as RunLogObj
 from application.model.run_log import *
 
 
@@ -20,25 +21,21 @@ def get_multi_batch_run_log_info(**kwargs):
     ret = []
     for table_name in table_name_fix_lst:
         table = get_batch_run_log_table(table_name)
-        if batch_id:
-            batch_list = [batch_id] if not isinstance(batch_id, list) else batch_id
-            if len(table_name_fix_lst) == 1 and to_time:
-                sql = table.select().where(table.c.end_time.__le__(to_time)).where(table.c.batch_id.in_(batch_list))
-                if from_time:
-                    sql.where(table.c.end_time.__ge__(from_time))
 
-            elif table_name == table_name_fix_lst[0] and from_time:
-                sql = table.select().where(table.c.end_time.__ge__(from_time)).\
-                    where(table.c.batch_id.in_(batch_list))
+        batch_list = [batch_id] if not isinstance(batch_id, list) else batch_id
 
-            elif table_name == table_name_fix_lst[-1] and to_time:
-                sql = table.select().where(table.c.end_time.__le__(to_time)). \
-                    where(table.c.batch_id.in_(batch_list))
-
-            else:
-                sql = table.select().where(table.c.batch_id.in_(batch_list))
+        if len(table_name_fix_lst) == 1 and to_time:
+            sql = table.select().where(table.c.end_time.__le__(to_time))
+            if from_time:
+                sql.where(table.c.end_time.__ge__(from_time))
+        elif table_name == table_name_fix_lst[0] and from_time:
+            sql = table.select().where(table.c.end_time.__ge__(from_time))
+        elif table_name == table_name_fix_lst[-1] and to_time:
+            sql = table.select().where(table.c.end_time.__le__(to_time))
         else:
             sql = table.select()
+        if batch_id:
+            sql = sql.where(table.c.batch_id.in_(batch_list))
         if limit:
             sql.limit(limit)
         ret += exec_query(sql, is_list=True)
@@ -77,27 +74,23 @@ def get_use_case_run_log(**kwargs):
     to_time = kwargs.get('to_time')
     ret = []
     for table_name in table_name_fix_lst:
-        table = get_batch_run_log_table(table_name)
-        if use_case_id:
-            use_case_list = [use_case_id] if not isinstance(use_case_id, list) else use_case_id
-            if len(table_name_fix_lst) == 1 and to_time:
-                sql = table.select().where(table.c.end_time.__le__(to_time)).\
-                    where(table.c.use_case_id.in_(use_case_list))
-                if from_time:
-                    sql.where(table.c.end_time.__ge__(from_time))
+        table = get_use_case_run_log_table(table_name)
+        use_case_list = [use_case_id] if not isinstance(use_case_id, list) else use_case_id
+        if len(table_name_fix_lst) == 1 and to_time:
+            sql = table.select().where(table.c.end_time.__le__(to_time))
+            if from_time:
+                sql.where(table.c.end_time.__ge__(from_time))
 
-            elif table_name == table_name_fix_lst[0] and from_time:
-                sql = table.select().where(table.c.end_time.__ge__(from_time)). \
-                    where(table.c.use_case_id.in_(use_case_list))
+        elif table_name == table_name_fix_lst[0] and from_time:
+            sql = table.select().where(table.c.end_time.__ge__(from_time))
 
-            elif table_name == table_name_fix_lst[-1] and to_time:
-                sql = table.select().where(table.c.end_time.__le__(to_time)). \
-                    where(table.c.use_case_id.in_(use_case_list))
+        elif table_name == table_name_fix_lst[-1] and to_time:
+            sql = table.select().where(table.c.end_time.__le__(to_time))
 
-            else:
-                sql = table.select().where(table.c.use_case_id.in_(use_case_list))
         else:
             sql = table.select()
+        if use_case_id:
+            sql = sql.where(table.c.use_case_id.in_(use_case_list))
         if limit:
             sql.limit(limit)
         ret += exec_query(sql, is_list=True)
@@ -113,8 +106,46 @@ def add_interface_run_log(**kwargs):
 
 @table_decorator
 def get_interface_run_log(**kwargs):
-    table = get_use_case_run_log_table(kwargs.pop('table_time')[0])
-    sql = table.insert(kwargs)
-    return exec_change(sql)
+    table_name_fix_lst = kwargs.pop('table_name_fix_lst')
+    interface_id = kwargs.get('interface_id')
+    limit = kwargs.get('limit')
+    from_time = kwargs.get('from_time')
+    to_time = kwargs.get('to_time')
+    ret = []
+    for table_name in table_name_fix_lst:
+        table = get_interface_run_log_table(table_name)
+        if interface_id:
+            interface_list = [interface_id] if not isinstance(interface_id, list) else interface_id
+            if len(table_name_fix_lst) == 1 and to_time:
+                sql = table.select().where(table.c.end_time.__le__(to_time)). \
+                    where(table.c.interface_id.in_(interface_list))
+                if from_time:
+                    sql.where(table.c.end_time.__ge__(from_time))
+
+            elif table_name == table_name_fix_lst[0] and from_time:
+                sql = table.select().where(table.c.end_time.__ge__(from_time)). \
+                    where(table.c.interface_id.in_(interface_list))
+
+            elif table_name == table_name_fix_lst[-1] and to_time:
+                sql = table.select().where(table.c.end_time.__le__(to_time)). \
+                    where(table.c.interface_id.in_(interface_list))
+
+            else:
+                sql = table.select().where(table.c.interface_id.in_(interface_list))
+        else:
+            sql = table.select()
+        if limit:
+            sql.limit(limit)
+        ret += exec_query(sql, is_list=True)
+
+
+@table_decorator
+def modify_interface_run_log(**kwargs):
+    table = get_interface_run_log_table(kwargs.pop('table_name_fix_lst')[0])
+    id = kwargs.pop('id')
+    sql = table.update(table.c.id == id).values(**kwargs)
+    return exec_change(sql).inserted_primary_key[0]
+
+
 
 
