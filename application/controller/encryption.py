bf0c075929408414c56b3212923b1da90649b37a
Yuan Liu <liuyuan900811@gmail.com>
2018-04-25 11:35:02 +0800

    加密方式url更新

--- application/controller/encryption.py
+++ application/controller/encryption.py
@@ -4,6 +4,6 @@ from application import app
 from application.api import encryption as EncryptionAPI
 
 
-@app.route('/get/encryption')
-def get_encryption():
+@app.route('/encryption/list')
+def encryption_list():
     return jsonify(EncryptionAPI.get_encryption_list())
