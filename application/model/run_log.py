d83249f1dc52eecb4b4f24dd535aabc2bf05762b
lichengbo <lichengbo@huishoubao.com.cn>
2018-04-27 17:13:22 +0800

    添加字符串长度为10000

--- application/model/run_log.py
+++ application/model/run_log.py
@@ -59,7 +59,7 @@ def get_interface_run_log_table(table_name):
                       Column('interface_id', Integer, nullable=False),
                       Column('r_code', String(1000)),
                       Column('r_header', String(1000)),
-                      Column('r_payload', String(1000)),
+                      Column('r_payload', String(10000)),
                       Column('is_pass', Boolean, nullable=False),
                       Column('cost_time', Float, nullable=False),
                       Column('start_time', TIMESTAMP(fsp=3), default=datetime.utcnow),
