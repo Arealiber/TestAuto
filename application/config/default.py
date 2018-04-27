e69852a7ba45d99809ec67a53b3d0eba36c1a0f4
Yuan Liu <liuyuan900811@gmail.com>
2018-04-27 11:10:08 +0800

    停止使用pymysql

--- application/config/default.py
+++ application/config/default.py
@@ -13,7 +13,7 @@ DEBUG = True
 HOST = '0.0.0.0'
 PORT = 8000
 
-DB_URI = 'mysql+pymysql://hjx:123456@119.29.141.207/auto_test'
+DB_URI = 'mysql://hjx:123456@119.29.141.207/auto_test'
 DB_POOL_SIZE = 100
 DB_POOL_RECYCLE = 7200
 DB_ECHO = False
