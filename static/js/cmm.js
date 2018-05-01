a85832fadea4348af8cd8300979ff0c342e1b06d
lichengbo <lichengbo@huishoubao.com.cn>
2018-05-01 16:12:03 +0800

    优化翻页等功能的界面

--- static/js/cmm.js
+++ static/js/cmm.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     $("#jump_page").click(function () {
+        var max_num = $('#jump_page').data('page_num');
+        var time_url = $('#jump_page').data('time_url');
         var page_num = $("#input_page").val();
         var link_url = window.location.href;
         var index = link_url.lastIndexOf("\/");
         link_url = link_url.substring(0, index+1);
-        window.location.href= link_url + page_num
+        if (page_num > max_num || !page_num){
+            window.location.href= link_url + max_num + time_url
+        }else {
+            window.location.href= link_url + page_num + time_url
+        }
     })
 });
 
