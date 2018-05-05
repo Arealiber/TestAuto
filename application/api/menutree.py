aeef366aced1f3ee3ead25ecc7a61dfad4397f4b
lichengbo <lichengbo@huishoubao.com.cn>
2018-05-05 18:06:44 +0800

    添加菜单数据库，新增前段treeview实现

---
+++ application/api/menutree.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8
+from application import session_scope
+from application.model.menutree import *
+
+
+def add_business_line(**kwargs):
+    with session_scope() as session:
+        business_line = BusinessLine(**kwargs)
+        session.add(business_line)
+        session.flush()
+        return business_line.id
+
+
+def add_system_line(**kwargs):
+    with session_scope() as session:
+        system_line = SystemLine(**kwargs)
+        session.add(system_line)
+        session.flush()
+        return system_line.id
+
+
+def add_function_line(**kwargs):
+    with session_scope() as session:
+        function_line = FunctionLine(**kwargs)
+        session.add(function_line)
+        session.flush()
+        return function_line.id
+
+
+def query_business_line(**kwargs):
+    with session_scope() as session:
+        query = session.query(BusinessLine).filter_by(**kwargs).filter_by(status=1)
+    business_list = [business.to_dict() for business in query]
+    return business_list
+
+
+def query_system_line(**kwargs):
+    with session_scope() as session:
+        query = session.query(SystemLine).filter_by(**kwargs).filter_by(status=1)
+    system_line_list = [system_line.to_dict() for system_line in query]
+    return system_line_list
+
+
+def query_function_line(**kwargs):
+    with session_scope() as session:
+        query = session.query(FunctionLine).filter_by(**kwargs).filter_by(status=1)
+    function_line_list = [function_line.to_dict() for function_line in query]
+    return function_line_list
+
+
+def query_function_line_of_system_line(system_line_id=None):
+    with session_scope() as session:
+        query = session.query()
+
+
+
+
--- static/js/cmm.js
+++ static/js/cmm.js
@@ -16,6 +16,92 @@ $(document).ready(function () {
     })
 });
 
+function getTree() {
+    var tree = [
+        {
+            text: "参数管理",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/parameter/1",
+            levels: 1,
+        },
+        {
+            text: "接口管理",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/interface/1",
+            levels: 1,
+        },
+        {
+            text: "用例管理",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/use_case/1",
+            levels: 1,
+        },
+        {
+            text: "批次管理",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/batch/1",
+            levels: 1,
+        },
+        {
+            text: "运行日志",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/use_case_run_log/1",
+            levels: 1,
+        },
+        {
+            text: "报表",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/#",
+            levels: 1,
+        },
+        {
+            text: "注销",
+            tags: ['available'],
+            color: "#8f9baa",
+            backColor: "transparent",
+            href: "/#",
+            levels: 1,
+            state:{
+                checked:false,
+                disabled:false,
+                expanded:false,
+                selected:false
+            }
+        },
+    ];
+    return tree
+};
+
+function getBaseUrl() {
+        var url = window.location.href;
+        var url_list = url.split('/');
+        var base_url = url_list[0] + '//' + url_list[1] + url_list[2];
+        return base_url
+}
+function getMapfromUrl(data) {
+    var base_url = getBaseUrl();
+    url = base_url+data.href;
+    if (data.state.selected) {
+        window.location.href = url;
+    }
+
+}
+
+// $('#menu_tree').treeview({data:getTree()});
+
+
 
 
 
