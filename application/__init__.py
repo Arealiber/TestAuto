74ec311b29940c7e028247b164ebbbbd2db850f9
Yuan Liu <liuyuan900811@gmail.com>
2018-05-10 11:00:53 +0800

    重新启用连接池并优化数据库连接

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,9 @@
 import os
+import gc
 from contextlib import contextmanager
 from flask import Flask
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from application.config import default
 
@@ -20,15 +20,15 @@ app.config.from_object(default)
 # SQLAlchemy engine
 engine = create_engine(app.config['DB_URI'] + '?charset=utf8',
                        encoding='utf-8',
-                       poolclass=NullPool,
                        convert_unicode=True,
-                       # pool_recycle=app.config['DB_POOL_RECYCLE'],
-                       # pool_size=app.config['DB_POOL_SIZE'],
+                       pool_recycle=app.config['DB_POOL_RECYCLE'],
+                       pool_size=app.config['DB_POOL_SIZE'],
+                       max_overflow=app.config['DB_MAX_OVERFLOW'],
                        echo=app.config['DB_ECHO'],
                        pool_pre_ping=True)
 
 # SQLAlchemy session
-session_maker = sessionmaker(bind=engine)
+session_maker = scoped_session(sessionmaker(bind=engine))
 
 
 @contextmanager
@@ -36,6 +36,7 @@ def session_scope():
     session = session_maker()
     try:
         yield session
+        session.expunge_all()
         session.commit()
     except:
         session.rollback()
