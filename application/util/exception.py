cf4ba767238a0a9d271bdaf17dc221cea0409b03
Yuan Liu <liuyuan900811@gmail.com>
2018-05-15 16:27:51 +0800

    暂时注释掉异常日志记录以免出错

--- application/util/exception.py
+++ application/util/exception.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from functools import wraps
 
-from application.util import logger as LOGGER
+# from application.util import logger as LOGGER
 
 
 def try_except(fn):
@@ -11,6 +11,6 @@ def try_except(fn):
             return fn(*args, **kwargs)
         except Exception as e:
             error_str = '{0}: {1}'.format(str(e.__class__.__name__), str(e))
-            LOGGER.exception_log(error_str)
+            # LOGGER.exception_log(error_str)
             return jsonify({'success': False, 'error': str(e)})
     return wrapped
