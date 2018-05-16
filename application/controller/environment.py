8723152f636da882b86bece8d8c6e634e214dc60
lichengbo <lichengbo@huishoubao.com.cn>
2018-05-16 11:49:31 +0800

    删除环境时，同时解除用例和环境的关系

--- application/controller/environment.py
+++ application/controller/environment.py
@@ -3,6 +3,7 @@ from flask import request, jsonify
 
 from application import app
 from application.api import environment as EnvironmentAPI
+from application.api import use_case as UseCaseAPI
 
 
 @app.route('/environment/line/add', methods=['POST'])
@@ -60,6 +61,9 @@ def del_environment_line():
     """
     try:
         environment_line_id = EnvironmentAPI.del_environment_line(**request.get_json())
+        use_case_info_list = UseCaseAPI.get_use_case(environment_id=request.get_json().get('id'))
+        for use_case_info in use_case_info_list:
+            UseCaseAPI.modify_use_case(id=use_case_info.get('id'), environment_id=0)
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
     return jsonify({'success': True, 'res': environment_line_id})
@@ -102,6 +106,3 @@ def get_environment_line_info():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
     return jsonify({'success': True, 'res': environment_info_list})
-
-
-
