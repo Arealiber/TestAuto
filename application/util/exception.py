98322fba43d7cbcddb85bd949493f290fd878d0c
Yuan Liu <liuyuan900811@gmail.com>
2018-05-30 10:07:41 +0800

    更新异常日志信息，现在包含function name和完整的traceback

--- application/util/exception.py
+++ application/util/exception.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import traceback
+
 from flask import jsonify
 from functools import wraps
 from application import app
@@ -12,10 +16,17 @@ def try_except(fn):
         try:
             return fn(*args, **kwargs)
         except Exception as e:
-            error_str = '{0}: {1}'.format(str(e.__class__.__name__), str(e))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            error_str = 'Function name: {0}; Error info: {1}: {2}; Traceback: {3}'\
+                .format(str(fn.__name__),
+                        str(e.__class__.__name__),
+                        str(e),
+                        str(traceback.extract_tb(exc_tb)))
+            # 正式环境，记录错误日志并返回错误信息
             if not app.config['DEBUG']:
                 LOGGER.exception_log(error_str)
+                return jsonify({'success': False, 'error': str(e)})
+            # 测试环境直接raise
             else:
                 raise
-            return jsonify({'success': False, 'error': str(e)})
     return wrapped
