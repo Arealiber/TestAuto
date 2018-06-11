4e6952d3834ddac35a52cdf373a7aafb01b1c734
lichengbo <lichengbo@huishoubao.com.cn>
2018-06-11 11:12:34 +0800

    添加延时函数，等待环境信息加载

--- static/js/cmm.js
+++ static/js/cmm.js
@@ -248,3 +248,8 @@ function getRandomColor() {
     }
     return color;
 }
+
+function sleep(n) {
+    var start = new Date().getTime();
+    while(true)  if(new Date().getTime()-start > n) break;
+}
