e53f08d60fc52ad22fd08c0356ef1868f74d6b9c
lichengbo <lichengbo@huishoubao.com.cn>
2018-06-25 15:45:17 +0800

    json数据显示格式化后字体设置

--- static/js/cmm.js
+++ static/js/cmm.js
@@ -264,7 +264,7 @@ function sleep(n) {
 
 function syntaxHighlight(json) {
     if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
+        json = JSON.stringify(json, undefined, 4);
     }
     json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
