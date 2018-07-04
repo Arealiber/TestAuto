74c071cbc313d393e7bead7552027ee7a75a8a5e
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-04 15:10:15 +0800

    更改数据库用户名

--- application/config/default.py
+++ application/config/default.py
@@ -25,7 +25,7 @@ DEBUG = False
 HOST = '0.0.0.0'
 PORT = 9091
 
-DB_URI = 'mysql://root:123456@10.251.101.163/auto_test'
+DB_URI = 'mysql://hjx:123456@10.251.101.163/auto_test'
 DB_POOL_SIZE = 5
 DB_POOL_RECYCLE = 5
 DB_MAX_OVERFLOW = 10
