5e2ba206f366c91dbba1428065c0f0fa6b64fd36
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-04 19:08:49 +0800

    添加git操作接口

--- application/api/git_option.py
+++ application/api/git_option.py
@@ -6,7 +6,7 @@ def create_tag(project_path, tag):
     print(os.popen('pwd').readlines())
     os.system('cd %s' % project_path)
     print(os.popen('pwd').readlines())
-    cmd_str = 'git tag %s' % tag
+    cmd_str = os.path.join(project_path, 'git tag %s' % tag)
     res = os.popen(cmd_str).readlines()
     os.popen('git push --tags').readline()
     # os.system('git tag -d %s' % tag)
