a9bfaa36a112c9507367c80bd301bd4e33dd587a
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-05 10:25:52 +0800

    添加git操作接口

--- application/controller/git_option.py
+++ application/controller/git_option.py
@@ -3,7 +3,7 @@ from flask import request, jsonify
 from application import app
 from application.util.exception import try_except
 from application.controller import login_required
-from application.api import git_option as gitAPI
+from application.util import git_option as gitAPI
 
 
 @app.route('/git/create_tag', methods=['POST'])
@@ -14,9 +14,14 @@ def create_tag():
     功能描述: 创建一个tag
     :return:
     """
-    tag = request.get_json()['tag']
-    result = gitAPI.create_tag(tag)
-    return jsonify({'success': True, 'res': result})
+    soft_name = request.get_json()['soft_name']
+    work_path = request.get_json()['work_path']
+    if not (soft_name or work_path):
+        return jsonify({'success': False, 'res': '参数错误'})
+    tag = gitAPI.create_tag(soft_name, work_path)
+    if not tag:
+        return jsonify({'success': False, 'res': '创建tag失败'})
+    return jsonify({'success': True, 'res': tag})
 
 
 
---
+++ application/util/git_option.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+import os
+import re
+
+
+def create_tag(soft_name, work_path):
+    work_path_cmd = 'cd %s' % work_path
+    tag_name = get_new_tag(soft_name, work_path)
+    git_tag_cmd = ';'.join([work_path_cmd, 'git tag %s' % tag_name])
+    git_pull_cmd = ';'.join([work_path_cmd, 'git pull'])
+    push_tag_cmd = ';'.join([work_path_cmd, 'git push origin %s' % tag_name])
+
+    os.system(git_pull_cmd)
+    os.system(git_tag_cmd)
+    res = os.system(push_tag_cmd)
+    if res == 0:
+        return tag_name
+    return False
+
+
+def get_all_tags(work_path):
+    work_path_cmd = 'cd %s' % work_path
+    show_tags_cmd = ';'.join([work_path_cmd, 'git tag'])
+    tags_list = os.popen(show_tags_cmd).readlines()
+    return tags_list
+
+
+def get_new_tag(soft_name, work_path):
+    tag_list = get_all_tags(work_path)
+    soft_tags_list = [tag.splitlines() for tag in tag_list if soft_name in tag]
+    if not soft_tags_list:
+        return '{0}-test-v0.0.1'.format(soft_name)
+    max_major_ver_num = int(max([re.match(r'v(\d).\d.\d', name)[0] for name in soft_tags_list]))
+    max_child_ver_num = int(max([re.match(r'v\d.(\d).\d', name)[0] for name in soft_tags_list]))
+    max_phase_num = int(max([re.match(r'v\d.\d.(\d)', name)[0] for name in soft_tags_list]))
+    if int(max_phase_num) < 9:
+        new_tag_name = '{0}-test-v{1}.{2}.{3}'.format(soft_name, max_major_ver_num, max_child_ver_num, max_phase_num+1)
+    elif max_child_ver_num < 9:
+        new_tag_name = '{0}-test-v{1}.{2}.0'.format(soft_name, max_major_ver_num, max_child_ver_num+1)
+    else:
+        new_tag_name = '{0}-test-v{1}.0.0'.format(soft_name, max_major_ver_num+1)
+    return new_tag_name
+
+
+
+
+
+
+
