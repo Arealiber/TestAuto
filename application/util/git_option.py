7a1a2fb98e314c563f22d4e5471e29e1094d07c9
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-05 10:53:12 +0800

    添加git操作接口

--- application/util/git_option.py
+++ application/util/git_option.py
@@ -29,15 +29,15 @@ def get_all_tags(work_path):
 def get_new_tag(soft_name, work_path):
     tag_list = get_all_tags(work_path)
     print(tag_list)
-    soft_tags_list = [tag.splitlines() for tag in tag_list if '{0}-test-v'.format(soft_name) in tag]
+    soft_tags_list = [tag.splitlines()[0] for tag in tag_list if '{0}-test-v'.format(soft_name) in tag]
     if not soft_tags_list:
         return '{0}-test-v0.0.1'.format(soft_name)
     major_pattern = re.compile(r'-test-v(\d).\d.\d')
     child_pattern = re.compile(r'-test-v\d.(\d).\d')
     phase_pattern = re.compile(r'-test-v\d.\d.(\d)')
-    max_major_ver_num = [major_pattern.findall(name)[0] for name in soft_tags_list]
-    max_child_ver_num = [child_pattern.findall(name)[0] for name in soft_tags_list]
-    max_phase_num = [phase_pattern.findall(name)[0] for name in soft_tags_list]
+    max_major_ver_num = max([major_pattern.findall(name)[0] for name in soft_tags_list])
+    max_child_ver_num = max([child_pattern.findall(name)[0] for name in soft_tags_list])
+    max_phase_num = max([phase_pattern.findall(name)[0] for name in soft_tags_list])
     print(max_major_ver_num, max_child_ver_num, max_phase_num)
     if int(max_phase_num) < 9:
         new_tag_name = '{0}-test-v{1}.{2}.{3}'.format(soft_name, max_major_ver_num, max_child_ver_num, max_phase_num+1)
