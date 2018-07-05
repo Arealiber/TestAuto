0e6913398023207c7d71d986a4f770c20b76ddeb
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-05 16:08:04 +0800

    添加git操作接口

--- application/util/git_option.py
+++ application/util/git_option.py
@@ -12,9 +12,8 @@ def create_tag(soft_name, work_path):
     git.pull()
     work_path_cmd = 'cd %s' % work_path
     tag_name = get_new_tag(soft_name, work_path)
-
-    option_cmd = ';'.join([work_path_cmd, 'git pull', 'git tag %s -m "创建版本发布测试tag"' % tag_name, 'git push origin %s' % tag_name])
-
+    option_cmd = ';'.join([work_path_cmd, 'git pull', 'git tag %s -m "创建版本发布测试tag"' % tag_name,
+                           'git push origin %s' % tag_name])
     res = os.system(option_cmd)
     if res == 0:
         return tag_name
@@ -33,14 +32,12 @@ def get_new_tag(soft_name, work_path):
     soft_tags_list = [tag.splitlines()[0] for tag in tag_list if '{0}-test-v'.format(soft_name) in tag]
     if not soft_tags_list:
         return '{0}-test-v0.0.1'.format(soft_name)
-    print(soft_tags_list)
+
     major_pattern = re.compile(r'-test-v(\d).\d.\d')
     max_major_ver_num = max([major_pattern.findall(name)[0] for name in soft_tags_list if major_pattern.findall(name)])
     child_pattern = re.compile(r'-test-v%d.(\d).\d' % int(max_major_ver_num))
-    print(child_pattern)
     max_child_ver_num = max([child_pattern.findall(name)[0] for name in soft_tags_list if child_pattern.findall(name)])
     phase_pattern = re.compile(r'-test-v%d.%d.(\d)' % (int(max_major_ver_num), int(max_child_ver_num)))
-    print(phase_pattern)
     max_phase_num = max([phase_pattern.findall(name)[0] for name in soft_tags_list if phase_pattern.findall(name)])
     if int(max_phase_num) < 9:
         new_tag_name = '{0}-test-v{1}.{2}.{3}'.format(soft_name, str(max_major_ver_num), str(max_child_ver_num),
