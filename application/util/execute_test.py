e1d9b1ba4c3c1a2171c4c8c5ed7da78f792159df
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-12 11:40:23 +0800

    分支切换回主分支

--- application/util/execute_test.py
+++ application/util/execute_test.py
@@ -176,7 +176,7 @@ def run_use_case(use_case_id, batch_log_id=None, environment_id=None, relation_i
 
         for interface in interface_list:
             # 添加延时运行接口
-            interface_delay = int(interface.get('interface_delay'))
+            interface_delay = int(interface.get('interface_delay', 0))
             if interface_delay > 0:
                 time.sleep(interface_delay)
             interface_name = interface.get('interface_name')
