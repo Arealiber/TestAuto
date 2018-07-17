2da52408e42fb1b07195d3f7977b06265677c054
lichengbo <lichengbo@huishoubao.com.cn>
2018-07-17 17:07:53 +0800

    添加日志错误写入本地文件

--- application/controller/run_log.py
+++ application/controller/run_log.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from datetime import datetime
 from application.config.default import QUERY_TIME_FMT
 from flask import request, jsonify
 from application.api import run_log as RunLogAPI
