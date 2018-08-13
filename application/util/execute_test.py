5284f59e1c5f8d5f7e1cb818975c91e15b3c633d
lichengbo <lichengbo@huishoubao.com.cn>
2018-08-13 17:50:11 +0800

    接口名称获取

--- application/util/execute_test.py
+++ application/util/execute_test.py
@@ -317,8 +317,11 @@ def run_use_case(use_case_id, batch_log_id=None, environment_id=None, relation_i
                 elif '_head' in json_payload:
                     if '_interface' in json_payload['_head']:
                         requested_interface = json_payload['_head']['_interface']
-            if not requested_interface and interface['interface_url']:
-                requested_interface = interface['interface_url'].split('//')[1].split('/')[0]
+            if not requested_interface:
+                try:
+                    requested_interface = url.split('//')[1].split('/')[0]
+                except:
+                    requested_interface = interface['interface_url'].split('//')[1].split('/')[0]
 
             # 日志内容
             interface_log_dict['s_header'] = header if header else ''
