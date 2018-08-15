19387b2adc61941aa471d3e2838c32eaa52f3204
lichengbo <lichengbo@huishoubao.com.cn>
2018-08-15 15:40:40 +0800

    进程安全优化

--- application/util/execute_test.py
+++ application/util/execute_test.py
@@ -42,7 +42,7 @@ def new_getaddrinfo(*args):
     cur_pid = os.getpid()
     g_dns_dict = g_DNS.get_dns()
     LOGGER.info_log('dns内容：{0},url:{1}'.format(g_dns_dict, url))
-    if cur_pid in g_DNS.get_dns() and url in g_DNS.get_dns()[cur_pid].keys():
+    if cur_pid in g_DNS.get_dns() and url in g_dns_dict[cur_pid].keys():
         new_dns = g_DNS.get_dns()[cur_pid]
 
         local_args = ('www.huishoubao.com.cn', args[1], args[2], args[3])
