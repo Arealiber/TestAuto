4069d3a29864962496541f3f0698b1e5f12707a2
lichengbo <lichengbo@huishoubao.com.cn>
2018-08-28 10:00:39 +0800

    新增阿里服务器配置文件

--- application/config/ali_config.py
+++ application/config/ali_config.py
@@ -1,29 +1,4 @@
 # -*- coding:utf-8 -*-
-import os
-
-TIME_FMT = '%Y-%m-%d %H:%M:%S.%f GMT'
-QUERY_TIME_FMT = '%Y-%m-%d %H:%M:%S.%f'
-TABLE_TIME_FMT = '%Y-%m-%d'
-TABLE_FMT = '%Y'
-CONSTANT_LEN = 10  # 7 按月分表, 10按日分表
-
-# 报表计算时间间隔 分、天、周、月
-MINUTE_TIME_LENGTH = 5
-DAY_TIME_LENGTH = 1
-WEEK_TIME_LENGTH = 1
-MOUTH_TIME_LENGTH = 30
-DEFAULT_TIME_LENGTH = 7  # 默认7 个单位（日、周、月）长度
-
-DAY_TIME_FMT = '%Y-%m-%d 00:00:00'
-MINUTE_TIME_FMT = '%Y-%m-%d %H:%M:00.000'
-MONTH_TIME_FMT = '%Y-%m-00 00:00:00.000'
-
-web_root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
-
-PROXIES = True
-DEBUG = False
-HOST = '0.0.0.0'
-PORT = 9091
 
 DB_URI = 'mysql://root:123456@localhost/auto_test'
 DB_POOL_SIZE = 5
