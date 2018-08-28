cfedfe2b9b5da77dcdf450bfcd9e80c984da6d23
lichengbo <lichengbo@huishoubao.com.cn>
2018-08-28 10:01:35 +0800

    新增阿里服务器配置文件

--- application/config/ali_config.py
+++ application/config/ali_config.py
@@ -1,13 +1,6 @@
 # -*- coding:utf-8 -*-
 
 DB_URI = 'mysql://root:123456@localhost/auto_test'
-DB_POOL_SIZE = 5
-DB_POOL_RECYCLE = 5
-DB_MAX_OVERFLOW = 10
-DB_ECHO = False
-
-# 正式环境内部权限系统
-SYSTEM_ID = '42'
 
 # redis配置
 REDIS_HOST = '127.0.0.1'
